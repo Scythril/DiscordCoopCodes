1559bfcd57400613362239591233ace3c9b29e0c
Joshua Johnson <kendrome@gmail.com>
2018-11-19 20:06:36 -0600

    Add project files.

---
+++ DiscordCoopCords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordCoopCords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
